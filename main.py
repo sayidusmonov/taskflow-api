dd930e2b283a86fa65bd01fde344606e2535c277
Sayid <usmanovsayid517@gmail.com>
2026-07-19 16:08:46 +0100

    Add DELETE endpoint for tasks

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Task(BaseModel): 
-    id: int 
+class Task(BaseModel):
+    id: int
     title: str
-    completed: bool = False 
+    completed: bool = False
 
 tasks = []
 
 @app.get("/tasks")
-
 def get_tasks(): 
-    return tasks 
+    return tasks
 
 @app.post("/tasks")
-def create_tasks(task: Task):
+def create_task(task: Task):
     tasks.append(task)
-    return task 
+    return task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int): 
+    for t in tasks: 
+        if t.id == task_id:
+            tasks.remove(t)
+            return{"message": f"Task {task_id} deleted"}
+    raise HTTPException(status_code=404, detail="Task not found")
+    
+        
